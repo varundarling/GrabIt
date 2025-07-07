50188bec5a28b44e3ae89b98d9140d3cd9d40bcf
Varun <varundarling7165@gmail.com>
2025-07-07 13:22:54 +0530

    Added border for Add_Item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.varun.grabit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
